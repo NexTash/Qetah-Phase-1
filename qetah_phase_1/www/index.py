1e9b74cdf446103a0f224d888efc23d225f3ca8f
irfanzahoor <irfanzahoor250@gmail.com>
2024-08-29 19:27:08 +0500

    fixes

--- qetah_phase_1/www/index.py
+++ qetah_phase_1/www/index.py
@@ -2,9 +2,10 @@ import frappe
 
 def get_context(context):
     blogs = frappe.get_all("Blog Post", filters={"custom_is_featured": 1}, fields=["*"])
-    
+    testimonials = frappe.get_all("Testimonial", ["*"] ,filters={"featured": 1})
     products = frappe.get_all("Product", ["*"] ,filters={"is_featured": 1}, order_by="modified asc")
     products = frappe.get_all('Product',filters={} , fields=['*'])
+    
 
     for product in products:
         product['variations'] = frappe.get_all(
@@ -19,13 +20,6 @@ def get_context(context):
             fields=['*'],
             filters={'parent': product['name']}  
         )
-
-
-
-
-
-
-    testimonials = frappe.get_all("Testimonial", ["*"] ,filters={"featured": 1}, order_by="modified asc")
 
     context.update({
         "blogs": blogs,
