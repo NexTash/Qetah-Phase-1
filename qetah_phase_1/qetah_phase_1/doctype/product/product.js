54ccb6e7003e31e5c0f98630009ca39ddf9f7bbc
rajaabdullaharshad <rajaabdullaharshad@gmail.com>
2024-10-18 17:39:23 +0500

    Inventry task

--- qetah_phase_1/qetah_phase_1/doctype/product/product.js
+++ qetah_phase_1/qetah_phase_1/doctype/product/product.js
@@ -1,8 +1,20 @@
-// Copyright (c) 2024, NexTash and contributors
-// For license information, please see license.txt
+frappe.ui.form.on('Product', {
+    refresh: function(frm) {
+    }
+});
 
-// frappe.ui.form.on("Product", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Product Variation', {
+    item_code: function(frm, cdt, cdn) {
+        const row = locals[cdt][cdn];
+        frappe.db.get_value('Bin', { item_code: row.item_code }, ['warehouse', 'stock_value'])
+        .then((result) => {
+            if (result && result.message) {
+                row.warehouse = result.message.warehouse;
+                row.remaining_items = result.message.stock_value;
+                frm.refresh_field('variations');
+            } else {
+                console.error('No Bin document found for item code:', row.item_code);
+            }
+        })
+    }
+});
